f992529866b3192aa3a250a44715e50805dedcf9
nikesh-2005 <nikesh4real@gmail.com>
2026-08-11 22:48:39 +0530

    quick sort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,23 @@
+def quickSort(ar,low,high):
+    if low>=high:
+        return;
+    pivot=quick(ar,low,high);
+    quickSort(ar,low,pivot-1);
+    quickSort(ar,pivot+1,high);
+
+
+def quick(ar,low,high):
+    pivot=ar[high];
+
+    i=low-1;
+    for j in range(low,high):
+        if ar[j]<=pivot:
+            i+=1;
+            ar[i],ar[j]=ar[j],ar[i];
+    ar[i+1],ar[high]=ar[high],ar[i+1];
+    return i+1;
+
+
+ar=[2,9,4,7,6,4,1];
+quickSort(ar,0,len(ar)-1);
+print(ar)
