526ed3a20df14f409ea6f30faf5fad22155a3bdb
nikesh-2005 <nikesh4real@gmail.com>
2026-08-18 22:52:59 +0530

    arrays

--- Array/largest_subarray.py
+++ Array/largest_subarray.py
@@ -1,15 +1,38 @@
+#Brute force
 def largestSubArray(ar,target):
+    n=len(ar)
+    count=0
+    for startindex in range(n):
+        for endindex in range(startindex,n):
+            sum=0
+            for i in range(startindex,endindex+1):
+                sum+=ar[i]
+            if sum==target:
+                count=max(count,endindex-startindex+1)
+
+    return count
+
+#Hashing
+
+def largestSubArray2(ar,target):
+    n=len(ar)
+    count=0
     sum=0
-    cnt=0
-    cur=0
-    for num in ar:
-        sum+=num
-        cur+=1
+    hash={}
+    for i in range(n):
+        sum+=ar[i]
         if sum==target:
-            sum=num
-            if cur>cnt:
-                cnt=cur
-            cur=1
-    return cnt
-nums = [-3, 2, 1]
-print(largestSubArray(nums,15))
+            count=i+1
+        dif=sum-target
+        
+        if dif in hash:
+            count=max(count,i-hash[dif])
+
+        if sum not in hash:
+            hash[sum]=i
+
+    return count
+
+   
+nums = [10, 5, 2, 7, 1, 9]
+print(largestSubArray2(nums,15))
